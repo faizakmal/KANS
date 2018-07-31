e1f4d39861a84df5ea602eccebe68881055cd246
Muni <husnimubarokalghifari@gmail.com>
2018-07-31 15:10:09 +0700

    connect.php

--- database/connect.php
+++ database/connect.php
@@ -2,11 +2,7 @@
 
 $dbServername = "localhost";
 $dbUsername   = "root";
-<<<<<<< HEAD
 $dbPassword   = "123123123";
-=======
-$dbPassword   = "";
->>>>>>> parent of 83598c2... ignore connect.php tracking
 $dbName       = "kansdb";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
